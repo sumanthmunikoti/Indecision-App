6a9318e423ba0196da3dc129c9a0c05efc6d6418
sumanthmunikoti <sumanthmunikoti1781989@gmail.com>
2020-06-04 17:54:32 -0400

    local storage used to save state

--- src/Counter.js
+++ src/Counter.js
@@ -5,7 +5,18 @@ class Counter extends React.Component {
   constructor(props) {
     super(props)
     this.state= {
-      count: props.count
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    const json = parseInt(localStorage.getItem('count'))
+    this.setState(() => ({ count : json }))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+        localStorage.setItem('count', this.state.count)
     }
   }
 
@@ -45,8 +56,8 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-    count : 0
-}
+// Counter.defaultProps = {
+//     count : 0
+// }
 
 export default Counter
--- src/app.js
+++ src/app.js
@@ -6,8 +6,34 @@ class IndecisionApp extends React.Component {
     super(props)
 
     this.state = {
-      options: props.options
+      options: []
     }
+  }
+
+  componentDidMount() {
+
+    try {
+      const json = localStorage.getItem('options')
+      const options = JSON.parse(json)
+      if (options) {
+        this.setState(() => ({ options }))
+      }
+    } catch (e) {
+      //Nothin
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options)
+      // console.log(json)
+      localStorage.setItem('options', json)
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
   }
 
   handlePick = () => {
@@ -47,7 +73,8 @@ class IndecisionApp extends React.Component {
           hasOptions={this.state.options.length > 0} />
         <Options 
           options={this.state.options} 
-          handleDeleteOptions={this.handleDeleteOptions}/>
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}/>
         <AddOption handleAddOption={this.handleAddOption}/>
       </div>
       
@@ -55,16 +82,23 @@ class IndecisionApp extends React.Component {
   }
 
 }
-IndecisionApp.defaultProps = {
-  options : []
-}
+
+// IndecisionApp.defaultProps = {
+//   options : []
+// }
 
 const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>No options to display</p>}
       {
-        props.options.map((option) => <Option key={option} optionText={option} />)
+        props.options.map((option) => 
+          <Option 
+            key={option} 
+            optionText={option} 
+            handleDeleteOption={props.handleDeleteOption}/>
+        )
       }
     </div>
   );
@@ -74,6 +108,12 @@ const Option = (props) => {
   return (
     <div>
       {props.optionText}
+        <button 
+        onClick={() => {
+          props.handleDeleteOption(props.optionText)
+        }}>
+          Delete
+        </button>
     </div>
   );
 }
@@ -117,6 +157,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option)
 
     this.setState(() => ({ error }))
+
+    if (!error) {
+      e.target.elements.option.value = ''
+    }
   }
 
   render() {
